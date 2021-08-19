a14734f16d249bf4c73fc58c21b0fb87357ad5a8
NB_POSTVENTA2\Matias <matiasmartelossi@hotmail.com>
2021-08-19 17:16:00 -0300

    Pantallas de consultas de Consumos y de facturas

--- ConsultasApp/forms.py
+++ ConsultasApp/forms.py
@@ -17,3 +17,26 @@ class ConsumoForm (forms.ModelForm):
         widgets ={
             "suministro": forms.Select(attrs={'class':'form-control',}),
         }
+
+class FacturaForm(forms.ModelForm):
+    class Meta:
+        model = Factura
+        fields = [
+            "suministro",
+            "detalle_documento" ,
+            "vencimiento_pri",
+            "importe_pri",
+            "vencimiento_seg",
+            "importe_seg",
+        ]
+        label={
+            "Suministro": "suministro",
+            "Documento": "detalle_documento" ,
+            "Pri_Vencimiento": "vencimiento_pri",
+            "Pri_Importe": "importe_pri",
+            "Seg_Vencimiento": "vencimiento_seg",
+            "Seg_importe": "importe_seg",
+        }
+        widgets ={
+            "suministro": forms.Select(attrs={'class':'form-control',}),
+        }
--- ConsultasApp/views.py
+++ ConsultasApp/views.py
@@ -1,31 +1,43 @@
 from django.shortcuts import render, HttpResponse
 from ConsultasApp.models import Consumo, Factura
 from OficinaVirtualApp.models import Suministro 
-from ConsultasApp.forms import ConsumoForm
+from ConsultasApp.forms import ConsumoForm, FacturaForm
+#from OficinaVirtualApp.forms i
 
 
 
 def consultaFacturas(request):
-    facturas = Factura.objects.all()
-    suministros = Suministro.objects.all()
+    facturas = FacturaForm(request.POST)
+    medidor = facturas.data.get("suministro")
+    lista_facturas = Factura.objects.filter(suministro = medidor)
+    #suministros = SuministroForm()
     lista ={"facturas": facturas,
-            "suministros": suministros
+            "datos": lista_facturas,
             }
-    
-
+    if request.method == 'POST' and facturas.is_valid():
+        medidor = facturas.data.get("suministro")
+        lista_facturas = Factura.objects.filter(suministro = medidor)
+        lista = {
+                "facturas": facturas,
+                "datos": lista_facturas,
+        }
+        
     return render(request, "ConsultasApp/facturas.html", lista)
 
 def consultaConsumos(request):
     consumo = ConsumoForm(request.POST)
-    if request.method == 'POST':
-        if consumo.is_valid():
-                datos_consumo = consumo.cleaned_data
-                print(datos_consumo.get("suministro"))
-
-    lista = {
-            "consumos": consumo
-            
+    medidor = consumo.data.get("suministro")
+    datos_consumo = Consumo.objects.filter(suministro = medidor)
+    lista={
+            "consumos": consumo,
+            "datos": datos_consumo,
     }
+    if request.method == 'POST' and consumo.is_valid():
+        medidor = consumo.data.get("suministro")
+        datos_consumo = Consumo.objects.filter(suministro = medidor)
+        print(datos_consumo.get("suministro"))
+
+  
     return render(request, "ConsultasApp/consumos.html", lista) 
     
 

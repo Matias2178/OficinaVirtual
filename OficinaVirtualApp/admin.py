8cdcd0a7689bdfa01f91ee8605f718bed0e85c90
NB_POSTVENTA2\Matias <matiasmartelossi@hotmail.com>
2021-08-19 17:17:46 -0300

    Se elimino datos innecesarios en la tabla cliente, para que queden relacionados con Users

--- OficinaVirtualApp/admin.py
+++ OficinaVirtualApp/admin.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from OficinaVirtualApp.models import Cliente, Suministro, Turno
 
-class ClienteAdmin (admin.ModelAdmin):
-    list_display =  ("apellido", "nombre", "calle", "numero" )
-    search_fields = ("apellido", "nombre")
-
-
-admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Cliente)
 admin.site.register(Suministro)
 admin.site.register(Turno)
 
--- OficinaVirtualApp/models.py
+++ OficinaVirtualApp/models.py
@@ -6,20 +6,14 @@ from django.contrib.auth.models import User
 
 class Cliente (models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null= True)
-    apellido = models.CharField(max_length=30)
-    nombre = models.CharField(max_length=40)
-    dni = models.IntegerField()
     calle = models.CharField(max_length=30)
     numero = models.IntegerField()
     piso = models.IntegerField(blank=True, null=True) #para que no sea un capmo requerido
     depto = models.CharField(max_length=2, blank=True, null=True)
     barrio = models.CharField(max_length=20)
-    email = models.EmailField()
     factura = models.BooleanField(default=False)
     
-    def __str__(self):
-        return '{} {}'.format(self.apellido, self.nombre)
-    
+        
 class Suministro (models.Model):
     TIPO = [
         ('RES', 'Residencial'),

1141c1eebef32525a97da15f7d35eac95b203da8
NB_POSTVENTA2\Matias <matiasmartelossi@hotmail.com>
2021-08-19 11:58:35 -0300

    Actualiza el estado de las alertas si fueron vistas o aceptadas

--- AvisosAlertasApp/views.py
+++ AvisosAlertasApp/views.py
@@ -11,4 +11,10 @@ def avisos(request):
 def alertas(request):
     cliente = request.user.id
     alertas = Alerta.objects.filter(cliente = cliente)
+    Alerta.objects.filter(cliente = cliente, estado = "EMI").update(estado = "VIS")
+    if request.method == "POST":
+        print("Aceptado")
+        Alerta.objects.filter(cliente = cliente, estado = "VIS").update(estado = "ACE")
+        return render(request, "AvisosAlertasApp/alertas.html", {"alertas": alertas})
+        
     return render(request, "AvisosAlertasApp/alertas.html", {"alertas": alertas})
